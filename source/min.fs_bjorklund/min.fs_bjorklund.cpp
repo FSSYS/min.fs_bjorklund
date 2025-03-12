3d03afa978595a65f9aedd2dcd34ed60dcb29fab
FSSYS <107515257+FSSYS@users.noreply.github.com>
2025-03-12 21:38:21 +0000

    fix bugs

--- source/min.fs_bjorklund/min.fs_bjorklund.cpp
+++ source/min.fs_bjorklund/min.fs_bjorklund.cpp
@@ -50,12 +50,20 @@ public:
         }
         return rhythm;
     }
-    void rotate(int phase, atoms* list) {
-        std::rotate(list->begin(), list->end() - phase, list->end());
-        return;
+    static atoms bbjorklund(const int pulse, const int step, const int phase) {
+        auto res = bbjorklund(pulse, step);
+        std::rotate(res.begin(), res.end() - modulo(phase, step), res.end());
+        return res;
+    }
+    static unsigned modulo(int value, unsigned m) {
+        int mod = value % (int)m;
+        if (mod < 0) {
+            mod += m;
+        }
+        return mod;
     }
 
-   message<> list{
+   message<threadsafe::yes> list{
     this, "list", "create rhythm with beats/steps/(phase)",
     MIN_FUNCTION
     {
@@ -67,8 +75,9 @@ public:
                 output.send(atoms(args[1], 1));
             }
             else {
-                auto res = bbjorklund(args[0], args[1]);
-                rotate((int)args[2] % (int)args[1], &res);
+                lock lock{ m_mutex };
+                auto res = bbjorklund(args[0], args[1], args[2]);
+                lock.unlock();
                 output.send(res);
             }
         }
@@ -81,15 +90,112 @@ public:
                 output.send(atoms(args[1], 1));
             }
             else {
-                output.send(bbjorklund(args[0], args[1]));
+                lock lock{ m_mutex };
+                auto res = bbjorklund(args[0], args[1]);
+                lock.unlock();
+                output.send(res);
             }
         }
 
         return{};
     }
 };
-
+private:
+    mutex m_mutex;
 };
 
 
 MIN_EXTERNAL(fs_bjorklund);
+
+
+/*
+class fs_nbjorklund : public object<fs_nbjorklund> {
+public:
+    MIN_DESCRIPTION{ "Nested Euclidean rhythm generator" };
+    MIN_TAGS{ "time" };
+    MIN_AUTHOR{ "FSSYS" };
+    MIN_RELATED{ "min.beat.pattern, link.beat, metro, tempo, drunk" };
+
+    inlet<>  input{ this, "(list) beats/steps or beats/steps/phase" };
+    outlet<> output{ this, "(list) rhythm as string of 1 and 0" };
+
+    static atoms bbjorklund(const int pulse, const int step) {
+        const int pauses = step - pulse;
+        int remainder = pauses % pulse;
+        int per_pulse = pauses / pulse;
+        int noskip = (remainder == 0) ? 0 : pulse / remainder;
+        int skipXTime = (noskip == 0) ? 0 : (pulse - remainder) / noskip;
+        atoms rhythm;
+        rhythm.reserve(step);
+        int count = 0;
+        int skipper = 0;
+        for (int i = 1; i <= step; i++) {
+            if (count == 0) {
+                rhythm.push_back(1);
+                count = per_pulse;
+                if (remainder > 0 && skipper == 0) {
+                    count++;
+                    remainder--;
+                    skipper = (skipXTime > 0) ? noskip : 0;
+                    skipXTime--;
+                }
+                else {
+                    skipper--;
+                }
+            }
+            else {
+                rhythm.push_back(0);
+                count--;
+            }
+        }
+        return rhythm;
+    }
+    static atoms bbjorklund(const int pulse, const int step, const int phase) {
+        auto res = bbjorklund(pulse, step);
+        std::rotate(res.begin(), res.end() - phase, res.end());
+        return res;
+    }
+
+    static atoms nbjorklund(const int toppulse, const int topstep, const int topphase, const int bpulse, const int bstep, const int bphase) {
+        auto top = bbjorklund(toppulse, topstep, topphase);
+
+    }
+
+    message<> list{
+     this, "list", "create rhythm with beats/steps/(phase)",
+     MIN_FUNCTION
+     {
+         if (args.size() == 3) {
+             if (args[0] == 0) {
+                 output.send(atoms(args[1], 0));
+             }
+             else if (args[0] == args[1]) {
+                 output.send(atoms(args[1], 1));
+             }
+             else {
+                 output.send(bbjorklund(args[0], args[1],args[2]));
+             }
+         }
+
+         else if (args.size() == 2) {
+             if (args[0] == 0) {
+                 output.send(atoms(args[1], 0));
+             }
+             else if (args[0] == args[1]) {
+                 output.send(atoms(args[1], 1));
+             }
+             else {
+                 output.send(bbjorklund(args[0], args[1]));
+             }
+         }
+
+         return{};
+     }
+    };
+
+};
+
+
+MIN_EXTERNAL(fs_nbjorklund);
+
+*/
